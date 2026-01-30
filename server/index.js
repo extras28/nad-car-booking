e87cc6881668836962064f2399249e0dfe45a949
dung.na1 <dung.na1@cmctelecom.vn>
2026-01-30 10:56:41 +0700

    Fix: Add Vercel builds config and conditional app.listen

--- server/index.js
+++ server/index.js
@@ -246,8 +246,9 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
-
+if (require.main === module) {
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+    });
+}
 module.exports = app;
